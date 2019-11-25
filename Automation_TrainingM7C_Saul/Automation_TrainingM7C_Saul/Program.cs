0f6f41045b5ce2ad9d9878f9b57af5c81f3a2650
Saúl García <saul.garcia@4thsource.com>
2019-11-25 17:07:01 -0600

    Add Project

---
+++ Automation_TrainingM7C_Saul/Automation_TrainingM7C_Saul/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_TrainingM7C_Saul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
